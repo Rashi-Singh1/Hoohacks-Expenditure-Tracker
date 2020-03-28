9884472d46cbe5e8331e7abcdcc7afea614338f6
Rashi Singh <rashi170101052@iitg.ac.in>
2020-03-28 23:00:46 +0530

    Text conversion working, image storage error persists

--- ExpenditureTracker/app/src/main/java/com/example/expendituretracker/MainActivity.java
+++ ExpenditureTracker/app/src/main/java/com/example/expendituretracker/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.expendituretracker;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    private Button scan_bill, add_manual_entry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scan_bill = findViewById(R.id.scan_bill);
+        add_manual_entry = findViewById(R.id.add_manual_entry);
+
+        scan_bill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanBillActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        add_manual_entry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanBillActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
